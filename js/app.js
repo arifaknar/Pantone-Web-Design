5debe188534fc3c23ee9d489926232040a1cd477
Arif AKNAR <arifaknar@gmail.com>
2021-07-25 00:07:16 +0300

    Update 25.07.2021

---
+++ js/app.js
@@ -0,0 +1,131 @@
+
+//DOM Erişim Tanımlamaları
+const menu = document.querySelector("#menu");
+const menuOpenButton = document.querySelector("#menu-open");
+const menuCloseButton = document.querySelector("#menu-close");
+const images = document.querySelector(".images");
+const arrowLeft = document.querySelector(".carousel-control-prevv");
+const arrowRight = document.querySelector(".carousel-control-nextt");
+const slideNumberElement = document.querySelector("#slide-number");
+const productArrowLeft = document.querySelector(".arrow-left");
+const productArrowRight = document.querySelector(".arrow-right");
+const productInfoElement=document.querySelector("#product-info");
+const menuItems=document.querySelector("#menu ul")
+
+const menuContents = {
+  kurumsal: "kurumsal",
+  kabiliyetlerimiz: [
+    "Çocuk Kitabı",
+    "Okul Kitabı",
+    "Sanatsal Kitap",
+    "Roman",
+    "Çizgi Roman",
+    "Boyama Kitabı",
+    "Dergi",
+    "Katalog",
+    "Takvim",
+    "Defter",
+    "Broşür",
+    "Ajanda",
+    "Tanıtım Ürünleri",
+  ],
+  urunlerimiz: "urunlerimiz",
+  bizdenhaberler: "bizdenhaberler",
+  iletisim: {
+    heading: "Genel Merkez",
+    adress: "Akçaburgaz Mah. 137.Sok. No:12 <br> Esenyurt/İstanbul/TÜRKİYE",
+    tel: "444 62 18",
+    fax: "+90 212 656 2926",
+    email: "info@imakofset.com.tr",
+    mapButtonText: "Haritada gör",
+    directionsButtonText: "Yol tarifi al",
+  },
+};
+
+const products = [
+  { no: "01", name: "Çocuk Kitabı", url: "#", backgrounColor: "264653" },
+  { no: "02", name: "Okul Kitabı", url: "#", backgrounColor: "2a9d8f" },
+  { no: "03", name: "Sanatsal Kitap", url: "#", backgrounColor: "e9c46a" },
+  { no: "04", name: "çizgi Roman", url: "#", backgrounColor: "f4a261" },
+  { no: "05", name: "Boyama Kitabı", url: "#", backgrounColor: "e76f51" },
+  { no: "06", name: "Sanatsal Kitap", url: "#", backgrounColor: "264653" },
+  { no: "07", name: "çizgi Roman", url: "#", backgrounColor: "2a9d8f" },
+  { no: "08", name: "Boyama Kitabı", url: "#", backgrounColor: "e9c46a" },
+  { no: "09", name: "Sanatsal Kitap", url: "#", backgrounColor: "f4a261" },
+  { no: "10", name: "çizgi Roman", url: "#", backgrounColor: "e76f51" },
+  { no: "11", name: "Boyama Kitabı", url: "#", backgrounColor: "264653" },
+  { no: "12", name: "Sanatsal Kitap", url: "#", backgrounColor: "2a9d8f" },
+  { no: "13", name: "çizgi Roman", url: "#", backgrounColor: "e9c46a" },
+  { no: "14", name: "Boyama Kitabı", url: "#", backgrounColor: "f4a261" },
+  { no: "15", name: "Boyama Kitabı", url: "#", backgrounColor: "e76f51" },
+  ];
+
+
+
+
+
+//UI sınıfına ait ui objesinin tanımlanması
+const ui = new Ui();
+
+// Menüyü Gösterme ve Gizleme
+const showMenuToUi = () => ui.showMenu();
+const hideMenuToUi = () => ui.hideMenu();
+
+//Home-left Slider Düğme Eventleri
+const arrowLeftSlideOne = () => {
+   ui.arrowLeftSlideOneUI()
+   
+};
+const arrowRightSlideOne= () => {
+ ui.arrowRightSlideOneUI()
+};
+
+//Product Slider İşlemleri
+const arrowRightProduct = () => {
+  ui.arrowRightProductUI();
+};
+const arrowLeftProduct = () => {
+  ui.arrowLeftProductUI();
+};
+
+//Tüm event listener tanımlamalarının sayfa açılışında yüklenmesi
+eventListeners();
+
+// Tüm Event Listenerlar
+function eventListeners() {
+  document.addEventListener("DOMContentLoaded", getAllProducts(products));
+  menu.addEventListener("mouseover", menuEvents);
+  menuOpenButton.addEventListener("click", showMenuToUi);
+  menuCloseButton.addEventListener("click", hideMenuToUi);
+  arrowLeft.addEventListener("click", arrowLeftSlideOne);
+  arrowRight.addEventListener("click", arrowRightSlideOne);
+  productArrowLeft.addEventListener("click", arrowLeftProduct);
+  productArrowRight.addEventListener("click", arrowRightProduct);
+  menuItems.addEventListener("click",hideMenuToUi)
+}
+
+//Menü item hover olayı
+function menuEvents(e) {
+  const menuName = e.target.id;
+  const condition =
+    menuName.trim() != "" &&
+    menuName != "iletisim" &&
+    menuName != "menu" &&
+    menuName != "firstMenuContent" &&
+    menuName != "secondMenuContent";
+
+  if (condition) {
+    ui.addMenuArrayContent(menuContents[menuName]);
+  } else if (menuName == "iletisim") {
+    ui.addContactContent(menuContents[menuName]);
+  }
+}
+
+
+
+//Ürünleri Arayüze Getirme
+function getAllProducts(products) {
+  ui.getProducts(products);
+}
+
+
---
+++ js/ui.js
@@ -0,0 +1,117 @@
+//Tanımlamalar
+const firstMenuContent = document.querySelector("#firstMenuContent");
+const secondMenuContent = document.querySelector("#secondMenuContent");
+const productsElement = document.querySelectorAll(".slides")[0];
+let productsLeft = 0;
+let slideNumber = 1;
+
+//Arayüzle ilgili Sınıf ve fonksiyonlar
+
+class Ui {
+  constructor() {}
+
+//   Menü Fonksiyonları
+
+  showMenu() {
+    $("#menu").toggleClass("hide");
+  }
+
+  hideMenu() {
+    $("#menu").toggleClass("hide");
+
+    firstMenuContent.innerHTML = "";
+    secondMenuContent.innerHTML = "";
+  }
+
+  addMenuArrayContent(content) {
+    firstMenuContent.innerHTML = "";
+    secondMenuContent.innerHTML = "";
+    console.log(content);
+
+    if (typeof content == "object") {
+      content.map((item, index) => {
+        if (index < 9) {
+          firstMenuContent.innerHTML =
+            firstMenuContent.innerHTML + `<li>${item}</li>`;
+        } else {
+          secondMenuContent.innerHTML =
+            secondMenuContent.innerHTML + `<li>${item}</li>`;
+        }
+      });
+    } else if (content != undefined) {
+      firstMenuContent.innerHTML = `<li>${content}</li>`;
+    }
+  }
+
+  addContactContent(content) {
+    secondMenuContent.innerHTML = "";
+    firstMenuContent.innerHTML = `
+        <div style="margin-bottom:20px" id="contact-heading"><h3 > ${content.heading} </h3></div>
+        <div id="contact-content"></div>
+        <li style="margin-bottom:40px" > ${content.adress}</li> 
+        <li><b>Tel:</b> ${content.tel}</li> 
+        <li><b>Faks:</b>  ${content.fax}</li>
+        <li style="margin-bottom:30px"><b>Eposta:</b><a href="mailto:${content.email}"> ${content.email}</a> </li>  
+        <a href="#" class="btn btn-green" style="margin-right:10px">${content.mapButtonText}</a>
+        <a href="#" class="btn btn-orange">${content.directionsButtonText}</a>
+        
+        `;
+  }
+
+
+// Sayfa Açılışında Ürünleri Getirme
+
+  getProducts(products) {
+    products.map((product) => {
+      productsElement.innerHTML += `
+            <div class="product-card" style="background-color:#${product.backgrounColor}">
+            <div class="product-no">${product.no}.</div>
+            <div class="product-name">${product.name}</div>
+            <div class="product-url"><a href="${product.url}">Detaylı bilgi al</a> </div>
+        </div>`;
+    });
+  }
+
+  //Slogan slider Düğme Fonksiyonları
+  arrowLeftSlideOneUI(){
+    slideNumber--;
+    if(slideNumber<1){
+     slideNumber=3
+    } 
+    
+    slideNumberElement.innerHTML = slideNumber;
+    
+  }
+  arrowRightSlideOneUI(){
+    slideNumber++;
+    if(slideNumber>3){
+      slideNumber=1;
+    } 
+    slideNumberElement.innerHTML = slideNumber;
+
+  }
+  // Ürün Slider Düğme Fonksiyonları
+
+  arrowLeftProductUI(){
+        if(productsLeft!=0)
+        {
+           productsLeft += 280;
+        productsElement.style.left = `${productsLeft}px`;
+        console.log(productsElement.style.left);
+        }
+       
+      }
+  
+    arrowRightProductUI(){
+      const condition=-((products.length-1)*270.6)
+      console.log(condition)
+          if(productsLeft>condition){
+            productsLeft -= 280;
+          productsElement.style.left = `${productsLeft}px`;
+          console.log(productsElement.style.left);
+          }
+         
+        }
+      
+}
+
